22ae4fbcc8b4972a998b967640f5a10a7c204ff3
shj960531 <107528968+shj960531@users.noreply.github.com>
2022-06-28 17:51:46 +0900

    Create Cotae19.java

---
+++ Cotae/Cotae19.java
@@ -0,0 +1,15 @@
+public class Cotae19 {
+    public static void main(String[] args) {
+        int n = 9;
+        String[] count = new String[n];
+
+        for (int i = 0; i < n; ++i) {
+            if (i % 2 == 0) {
+                count[i] = "수";
+            } else {
+                count[i] = "박";
+            }
+            System.out.print(count[i]);
+        }
+    }
+}
