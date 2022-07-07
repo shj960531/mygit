02c59eaa65883ed73d97a86eeead6b9e995a0597
shj960531 <107528968+shj960531@users.noreply.github.com>
2022-07-07 20:20:27 +0900

    Create Timestamped.java

---
+++ Board/src/main/java/com/example/week03/domain/Timestamped.java
@@ -0,0 +1,16 @@
+package com.example.week03.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
